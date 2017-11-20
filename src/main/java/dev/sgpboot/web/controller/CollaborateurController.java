17bae8d0efbfb7f0abffb8cd097ab8a5a880f27b
Julien <j.jrohou@gmail.com>
2017-11-20 22:55:23 +0100

    20/11/17

--- src/main/java/dev/sgpboot/web/controller/CollaborateurController.java
+++ src/main/java/dev/sgpboot/web/controller/CollaborateurController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,11 @@ public class CollaborateurController {
 		
 		return this.listeCollaborateur.findAll();
 		
+	}
+	
+	@PutMapping(value="/{id}")
+	public Collaborateur editCollaborateur(@RequestBody Collaborateur editCollaborateur, @PathVariable Integer id) {
+		return this.listeCollaborateur.save(editCollaborateur);
 	}
 
 }
